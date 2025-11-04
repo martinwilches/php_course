20392f3f7c77d88b48a82d8cf0989f65544ab459
Martin Wilches <martin.devsoft@gmail.com>
2025-11-04 11:28:49 -0500

    Refactor database.php

--- __feedback/config/database.php
+++ __feedback/config/database.php
@@ -1,15 +1,14 @@
 <?php
 
 define('DB_HOST', 'localhost');
+define('DB_NAME', 'php_dev');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'php_dev');
 
-
-// creacion de la conexion
+// creacion de la conexion a la base de datos
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-// verificar la conexion
+// verificar que la conexion se establezca correctamente
 if ($conn->connect_error) {
     die('Connection failed ' . $conn->connect_error);
 }
